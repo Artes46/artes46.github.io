bc83cc82c214516fa26b508831efdad33fffbfdf
Artes46 <artemov46@gmail.com>
2020-08-07 17:17:57 +0300

    -

--- artemeow/js/mobile-menu.js
+++
@@ -1,8 +0,0 @@
-var nav = document.querySelector(".main-nav");
-var button = document.querySelector(".main-nav__toggle");
-
-button.addEventListener("click", function(event) {
-  event.preventDefault();
-  nav.classList.toggle("main-nav--opened");
-  button.classList.toggle("main-nav__toggle--open");
-});
